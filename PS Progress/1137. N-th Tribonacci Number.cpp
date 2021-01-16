59e243fa2b5159e309ed293e376f3452ab661c82
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-01-16 11:04:20 -0800

    Add files via upload

---
+++ PS Progress/1137. N-th Tribonacci Number.cpp
@@ -0,0 +1,14 @@
+// Question Link: https://leetcode.com/problems/n-th-tribonacci-number/
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        vector<int> dp(n + 4, 0);
+        dp[0] = 0;
+        dp[1] = dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+        return dp[n];
+    }
+};

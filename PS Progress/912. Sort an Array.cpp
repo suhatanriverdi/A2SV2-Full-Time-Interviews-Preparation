5877c2e34447d6000cac6003848bc898b0e198b3
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-01-17 07:57:27 -0800

    Add files via upload

---
+++ PS Progress/912. Sort an Array.cpp
@@ -0,0 +1,73 @@
+// Question Link: https://leetcode.com/problems/sort-an-array/
+
+class Solution {
+public:
+    // Merges two arrays by sorting 
+    void mergeArray(vector<int> &A, int left, int mid, int right) {
+        // Create temp arrays
+        int Lsize = mid - left + 1;
+        int Rsize = right - mid;
+        vector<int> L(Lsize);
+        vector<int> R(Rsize);
+        // Copy unsorted data from A to temp arrays
+        for (int i = 0; i < Lsize; i++) {
+            L[i] = A[left + i];
+        }
+        for (int j = 0; j < Rsize; j++) {
+            R[j] = A[mid + j + 1];
+        }
+        // Merge temp arrays back into A
+        int l = 0; // Left Temp Array
+        int r = 0; // Right Temp Array
+        int k; // Merged Original Array
+        for (k = left; k <= right && l < Lsize && r < Rsize; k++) {
+            if (L[l] <= R[r]) {
+                A[k] = L[l];
+                l++;
+            } else {
+                A[k] = R[r];
+                r++;
+            }
+        }
+        while (k <= r && l < Lsize && r < Rsize) {
+            if (L[l] <= R[r]) {
+                A[k] = L[l];
+                l++;
+            } else {
+                A[k] = R[r];
+                r++;
+            }
+            k++;
+        }
+        // Copy If Left Temp has any remaining elements
+        while (l < Lsize) {
+            A[k] = L[l];
+            l++;
+            k++;
+        }
+        // Copy If Right Temp has any remaining elements
+        while (r < Rsize) {
+            A[k] = R[r];
+            r++;
+            k++;
+        }
+    }
+
+    // Merge sort
+    void mergeSort(vector<int> &A, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            // Left Sub-Array
+            mergeSort(A, left, mid);
+            // Right Sub-Array
+            mergeSort(A, mid + 1, right);
+            // Merge Sub-Arrays
+            mergeArray(A, left, mid, right);
+        }
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums, 0, nums.size() - 1);
+        return nums;
+    }
+};

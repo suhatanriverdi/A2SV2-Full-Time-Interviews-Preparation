8dd540ff1a892e5ccd9b5fb7c7b074581ca94642
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-01-19 21:52:45 +0300

    Update 912. Sort an Array.cpp

--- PS Progress/912. Sort an Array.cpp
+++ PS Progress/912. Sort an Array.cpp
@@ -1,5 +1,27 @@
 // Question Link: https://leetcode.com/problems/sort-an-array/
 
+// Counting Sort Solution
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        int counts[100001] = {0};
+        // Maps negatives starting from 0
+        for (int &num : nums) {
+            counts[num + 50000]++;
+        }
+        vector<int> result(nums.size(), 0);
+        for (int i = 0, j = 0; i < 100001; i++) {
+            while (counts[i] > 0) {
+                result[j] = i - 50000;
+                j++;
+                counts[i]--;
+            }
+        }
+        return result;
+    }
+};
+
+// Merge Sort Solution
 class Solution {
 public:
     // Merges two arrays by sorting 

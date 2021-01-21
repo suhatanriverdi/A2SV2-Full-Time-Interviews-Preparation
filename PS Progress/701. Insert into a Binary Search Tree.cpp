17363fec9657f7452b9ca15171aef0892be2f526
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-01-21 08:57:14 -0800

    Add files via upload

---
+++ PS Progress/701. Insert into a Binary Search Tree.cpp
@@ -0,0 +1,17 @@
+// Question Link: https://leetcode.com/problems/insert-into-a-binary-search-tree
+
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if (root == NULL) {
+            return new TreeNode(val);
+        }
+        if (val > root->val) {
+            root->right = insertIntoBST(root->right, val);
+        }
+        else if (val < root->val) {
+            root->left = insertIntoBST(root->left, val);
+        }
+        return root;
+    }
+};

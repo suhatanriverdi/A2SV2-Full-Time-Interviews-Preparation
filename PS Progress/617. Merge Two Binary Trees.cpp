0fd3ae84b8c8dc8e0c4b017ffc2efe7f00509f5c
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-01-21 08:47:48 -0800

    Add files via upload

---
+++ PS Progress/617. Merge Two Binary Trees.cpp
@@ -0,0 +1,17 @@
+// Question Link: https://leetcode.com/problems/merge-two-binary-trees
+
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if (t1 == NULL) {
+            return t2;
+        }
+        if (t2 == NULL) {
+            return t1;
+        }
+        t1->val += t2->val;
+        t1->left = mergeTrees(t1->left, t2->left);
+        t1->right = mergeTrees(t1->right, t2->right);
+        return t1;
+    }
+};

eb8b6c4bc8467640e2b302699ecce0a4c4417e64
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-01-21 08:36:53 -0800

    Update 938. Range Sum of BST.cpp

--- PS Progress/938. Range Sum of BST.cpp
+++ PS Progress/938. Range Sum of BST.cpp
@@ -1,5 +1,35 @@
 // Question Link: https://leetcode.com/problems/range-sum-of-bst/
 
+// Solution 2 (Better)
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        int result = 0;
+        traverse(root, low, high, result);
+        return result;
+    }
+    
+    void traverse(TreeNode* cur, int &l, int &h, int &result) {
+        if (cur == NULL) {
+            return;
+        }
+        // Check if no need to go left, if we are less than L
+        if (cur->val < l) {
+            return traverse(cur->right, l, h, result);
+        }
+        // Check if no need to go right, if we are higher than H
+        if (cur->val > h) {
+            return traverse(cur->left, l, h, result);
+        }
+        if (cur->val >= l && cur->val <= h) {
+            result += cur->val;
+        }
+        traverse(cur->left, l, h, result);
+        traverse(cur->right, l, h, result);
+    }
+};
+
+// Solution 1
 class Solution {
 public:
     int rangeSumBST(TreeNode* root, int low, int high) {

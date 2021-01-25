4def4da0adc3aa5b23ac1825774008f3736ef871
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-01-25 18:32:53 +0300

    Update 347. Top K Frequent Elements.cpp

--- PS Progress/347. Top K Frequent Elements.cpp
+++ PS Progress/347. Top K Frequent Elements.cpp
@@ -1,5 +1,31 @@
 // Question Link: https://leetcode.com/problems/top-k-frequent-elements
 
+// O(m + k)
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> Map; // Counts
+        int maxFreq = 0;
+        for (int &n : nums) {
+            Map[n]++;
+            maxFreq = max(maxFreq, Map[n]);
+        }
+        vector<vector<int>> counts(maxFreq + 1);
+        for (auto it : Map) {
+            counts[it.second].push_back(it.first);
+        }
+        vector<int> result;
+        for (int i = counts.size() - 1; i >= 0 && k; i--) {
+            k -= counts[i].size();
+            while (counts[i].size() > 0) {
+                result.push_back(counts[i].back());
+                counts[i].pop_back();
+            }
+        }
+        return result;
+    }
+};
+
 // O(log k)
 class Solution {
 public:

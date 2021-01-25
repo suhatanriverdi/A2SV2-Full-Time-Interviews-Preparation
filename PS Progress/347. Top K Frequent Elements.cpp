19ca45b0ff04dc639073fb79987044e5ea55fa4d
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-01-25 18:02:41 +0300

    Update 347. Top K Frequent Elements.cpp

--- PS Progress/347. Top K Frequent Elements.cpp
+++ PS Progress/347. Top K Frequent Elements.cpp
@@ -3,30 +3,23 @@
 // O(log k)
 class Solution {
 public:
-    vector<string> topKFrequent(vector<string>& words, int k) {
-        unordered_map<string, int> Map; // Counts
-        auto comp = [](pair<int, string> &a, pair<int, string> &b) {
-            if (a.first != b.first) {
-                return a.first > b.first;
-            }
-            return a.second < b.second;
-        };
-        priority_queue<pair<int, string>, vector<pair<int, string>>, decltype(comp)> PQ(comp);
-        for (string &word : words) {
-            Map[word]++;
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> Map; // Counts
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> PQ; // Min Heap
+        for (int &n : nums) {
+            Map[n]++;
         }
         for (auto it : Map) {
             PQ.push({it.second, it.first});
             if (PQ.size() > k) {
                 PQ.pop();
-            }
+            }  
         }
-        vector<string> result;
+        vector<int> result;
         while (!PQ.empty()) {
-            result.push_back(PQ.top().second);
+            result.push_back(PQ.top()[1]);
             PQ.pop();
         }
-        reverse(begin(result), end(result));
         return result;
     }
 };

408c67ec23b66c8fc2f10774ab146b404d28ffaa
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-01-29 11:40:45 +0300

    Add files via upload

--- PS Progress/704. Binary Search.cpp
+++ PS Progress/704. Binary Search.cpp
@@ -1,17 +1,39 @@
-// Question Link: https://leetcode.com/problems/binary-search/submissions/
+// Question Link: https://leetcode.com/problems/binary-search
 
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1, mid;
-        while (left <= right) {
-            mid = (left + right) / 2;
+        int l = 0, r = nums.size() - 1, mid;
+        while (l <= r) {
+            mid = (l + r) / 2;
             if (nums[mid] == target) {
                 return mid;
-            } else if (target < nums[mid]) {
-                right = mid - 1;
+            }
+            else if (nums[mid] > target) {
+                r = mid - 1;
             } else {
-                left = mid + 1;
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
+
+
+// OverflowProof Version
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1, mid;
+        while (l <= r) {
+            mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] > target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
             }
         }
         return -1;

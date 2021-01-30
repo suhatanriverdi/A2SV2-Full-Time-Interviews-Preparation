6995de501ea5d8e325e8d6ecb20eed5b4a4f67ad
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-01-30 16:25:04 +0300

    Update 145. Binary Tree Postorder Traversal.cpp

--- PS Progress/145. Binary Tree Postorder Traversal.cpp
+++ PS Progress/145. Binary Tree Postorder Traversal.cpp
@@ -1,5 +1,31 @@
 // Question Link 1: https://leetcode.com/problems/binary-tree-postorder-traversal/
 
+// Iterative Solution
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        if (root == NULL) {
+            return result;
+        }
+        stack<TreeNode*> nodeStack;
+        nodeStack.push(root);
+        while (!nodeStack.empty()) {
+            TreeNode* cur = nodeStack.top();
+            nodeStack.pop();
+            result.push_back(cur->val);
+            if (cur->right) {
+                nodeStack.push(cur->right);
+            }
+            if (cur->left) {
+                nodeStack.push(cur->left);
+            }
+        }
+        return result;
+    }
+};
+
+// Recursive Solution
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {

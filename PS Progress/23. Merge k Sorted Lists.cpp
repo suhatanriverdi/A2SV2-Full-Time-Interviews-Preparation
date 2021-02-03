6e9ed44516ffa531e4cd573081915924260984c9
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-02-03 19:54:35 +0300

    Update 23. Merge k Sorted Lists.cpp

--- PS Progress/23. Merge k Sorted Lists.cpp
+++ PS Progress/23. Merge k Sorted Lists.cpp
@@ -1,5 +1,35 @@
 // Question Link: https://leetcode.com/problems/merge-k-sorted-lists/
 
+// Min Heap Solution
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>, greater<pair<int, ListNode*>>> minHeap;
+        for (ListNode* &head : lists) {
+            if (head) {
+                minHeap.push({head->val, head});
+            }
+        }
+        ListNode* root = new ListNode();
+        ListNode* walk = root;
+        while (!minHeap.empty()) {
+            // Get the minimum element from the heap
+            pair<int, ListNode*> cur = minHeap.top();
+            minHeap.pop();
+            
+            // Add the minimum element to the result
+            walk->next = new ListNode(cur.second->val);
+            walk = walk->next;
+            
+            // Update the pointer and push it back to minHeap
+            if (cur.second->next) {
+                minHeap.push({cur.second->next->val, cur.second->next});
+            }
+        }
+        return root->next;
+    }
+};
+
 // Counting Sort Solution
 class Solution {
 public:

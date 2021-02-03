f608ca6fc44c951e798c16e7bd6d866c8d085175
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-02-03 18:59:59 +0300

    Update 23. Merge k Sorted Lists.cpp

--- PS Progress/23. Merge k Sorted Lists.cpp
+++ PS Progress/23. Merge k Sorted Lists.cpp
@@ -1,5 +1,27 @@
 // Question Link: https://leetcode.com/problems/merge-k-sorted-lists/
 
+// Counting Sort Solution
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        vector<int> counts(20001, 0);
+        for (ListNode* &list : lists) {
+            for (ListNode* walk = list; walk; walk = walk->next) {
+                counts[walk->val + 10000]++;
+            }
+        }
+        ListNode* root = new ListNode();
+        ListNode* walk = root;
+        for (int i = 0; i < counts.size(); i++) {
+            while (counts[i]--) {
+                walk->next = new ListNode(i - 10000);
+                walk = walk->next;
+            }
+        }
+        return root->next;
+    }
+};
+
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {

3fbc44adf657a65b94b8677ddc210c7ff8e37601
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-02-10 08:51:06 -0800

    Add files via upload

---
+++ PS Progress/529. Minesweeper.cpp
@@ -0,0 +1,69 @@
+// Question Link: https://leetcode.com/problems/minesweeper
+
+class Solution {
+private:
+    vector<vector<int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+public:
+    vector<vector<char>> updateBoard(vector<vector<char>>& board, vector<int>& click) {
+        // If mine is at the click position, gave is over
+        if (board[click[0]][click[1]] == 'M') {
+            board[click[0]][click[1]] = 'X';
+            return board;
+        } 
+        // If there is any M around us, we only need to turn our cell into digit
+        checkCellsAround(board, click);
+        if (isdigit(board[click[0]][click[1]])) {
+            return board;
+        }
+        // Otherwise, run bfs to expand
+        else {
+            bfs(board, click);
+        }
+        return board;
+    }
+    
+    void bfs(vector<vector<char>> &board, vector<int> &click) {
+        // // First process ourself
+        // checkCellsAround(board, click);
+        queue<vector<int>> todo;
+        todo.push(click);
+        while (!todo.empty()) {
+            vector<int> cur = todo.front();
+            todo.pop();
+            // Process our neighbours
+            for (vector<int> &dir : dirs) {
+                int nx = dir[0] + cur[0];
+                int ny = dir[1] + cur[1];
+                if (nx >= 0 && nx < board.size() && ny >= 0 && ny < board[0].size() && board[nx][ny] == 'E') {
+                    vector<int> cell = {nx, ny};
+                    checkCellsAround(board, cell);
+                    // After process around us, check if our recently processed cell is Blank, if so push into queue
+                    if (board[nx][ny] == 'B') {
+                        todo.push(cell);
+                    }
+                }
+            }
+        }
+    }
+    
+    // Turns a cell into either digit or blank cell by looking around itself
+    void checkCellsAround(vector<vector<char>> &board, vector<int> &cell) {
+        int minesAroundCount = 0;
+        for (vector<int> &dir : dirs) {
+            int nx = dir[0] + cell[0];
+            int ny = dir[1] + cell[1];
+            // Check if we are inside of the array and there are mines around us, if so count them
+            if (nx >= 0 && nx < board.size() && ny >= 0 && ny < board[0].size() && board[nx][ny] == 'M') {
+                minesAroundCount++;
+            }
+        }
+        // If there is at least one mine, we have to turn our cell into digit cell
+        if (minesAroundCount > 0) {
+            board[cell[0]][cell[1]] = '0' + minesAroundCount;
+        }
+        // Else we can turn our cell into blank cell
+        else {
+            board[cell[0]][cell[1]] = 'B';
+        }
+    }
+};

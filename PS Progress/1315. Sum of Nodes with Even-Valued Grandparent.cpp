68e269cc68011d1761547023733d383fd55869d4
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-02-12 19:08:18 +0300

    Update 1315. Sum of Nodes with Even-Valued Grandparent.cpp

--- PS Progress/1315. Sum of Nodes with Even-Valued Grandparent.cpp
+++ PS Progress/1315. Sum of Nodes with Even-Valued Grandparent.cpp
@@ -1,5 +1,6 @@
 // Question Link: https://leetcode.com/problems/sum-of-nodes-with-even-valued-grandparent/
 
+// Solution 1
 class Solution {
 public:
     int sumEvenGrandparent(TreeNode* root) {
@@ -24,5 +25,40 @@ public:
         }
         helper(cur->left, sum);
         helper(cur->right, sum);
+    }
+};
+
+// Solution 2
+class Solution {
+public:
+    int sumEvenGrandparent(TreeNode* root) {
+        return helper(root, 1, 1);
+    }
+    
+    int helper(TreeNode* cur, int parent, int grandParent) {
+        if (cur == NULL) {
+            return 0;
+        }
+        int curContribution = (!(grandParent & 1) ? cur->val : 0);
+        return curContribution + helper(cur->left, cur->val, parent) + helper(cur->right, cur->val, parent);
+    }
+};
+
+// Solution 3
+class Solution {
+public:
+    int sumEvenGrandparent(TreeNode* root) {
+        int sum = 0;
+        helper(root, 1, 1, sum);
+        return sum;
+    }
+    
+    void helper(TreeNode* cur, int parent, int grandParent, int &sum) {
+        if (cur == NULL) {
+            return;
+        }
+        sum += (!(grandParent & 1) ? cur->val : 0);
+        helper(cur->left, cur->val, parent, sum);
+        helper(cur->right, cur->val, parent, sum);
     }
 };

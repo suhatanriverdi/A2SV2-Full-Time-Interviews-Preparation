201ab00d510980e2946be277fab823ba2ebd945a
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-03-06 17:42:26 +0300

    Update 230. Kth Smallest Element in a BST.cpp

--- PS Progress/230. Kth Smallest Element in a BST.cpp
+++ PS Progress/230. Kth Smallest Element in a BST.cpp
@@ -1,5 +1,31 @@
 // Question Link: https://leetcode.com/problems/kth-smallest-element-in-a-bst
 
+// With Early Termination
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        int kthElement;
+        bool found = false;
+        helper(root, k, kthElement, found);
+        return kthElement;
+    }
+    
+    void helper(TreeNode* cur, int &k, int &kthElement, bool &found) {
+        if (cur == NULL || found) {
+            return;
+        }
+        helper(cur->left, k, kthElement, found);
+        k--;
+        if (k == 0) {
+            found = true;
+            kthElement = cur->val;
+            return;
+        }
+        helper(cur->right, k, kthElement, found);
+    }
+};
+
+// Without Early Termination
 class Solution {
 public:
     int kthSmallest(TreeNode* root, int k) {

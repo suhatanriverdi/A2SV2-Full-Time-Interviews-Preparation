61bbf2c49f29a70d05664e66041e94c091e1f3cd
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-04-23 21:58:38 +0300

    Update 677. Map Sum Pairs.cpp

--- PS Progress/677. Map Sum Pairs.cpp
+++ PS Progress/677. Map Sum Pairs.cpp
@@ -1,5 +1,47 @@
 // Question Link: https://leetcode.com/problems/map-sum-pairs/
 
+// Trie Prefix Sum Solution
+struct TrieNode {
+    TrieNode* kids[26] = { NULL };
+    bool isWord = false;
+    int sum = 0;
+};
+
+class MapSum {
+public:
+    TrieNode *root = new TrieNode();
+    unordered_map<string, int> wordPrevSum;
+    MapSum() {
+    }
+    
+    void insert(string key, int val) {
+        int prevSum = (wordPrevSum.find(key) != wordPrevSum.end()) ? wordPrevSum[key] : 0;
+        wordPrevSum[key] = val;
+        TrieNode *cur = root;
+        for (char c : key) {
+            if (cur->kids[c - 'a'] == NULL) {
+                cur->kids[c - 'a'] = new TrieNode();
+            }
+            cur->kids[c - 'a']->sum += val;
+            cur->kids[c - 'a']->sum -= prevSum;
+            cur = cur->kids[c - 'a'];
+        }
+        cur->isWord = true;
+    }
+    
+    int sum(string prefix) {
+        TrieNode *cur = root;
+        for (char c : prefix) {
+            if (cur->kids[c - 'a'] == NULL) {
+                return 0;
+            }
+            cur = cur->kids[c - 'a'];
+        }
+        return cur->sum;
+    }
+};
+
+// Alphabetic Map Solution
 class MapSum {
 public:
     // Each map holds the words that starts with one specific letter

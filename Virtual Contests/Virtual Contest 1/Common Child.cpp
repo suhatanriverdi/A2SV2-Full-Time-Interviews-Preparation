21627a7cc96760d017ab1404efb7ef7b00ba4d60
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-04-25 00:26:47 +0300

    Update Common Child.cpp

--- Virtual Contests/Virtual Contest 1/Common Child.cpp
+++ Virtual Contests/Virtual Contest 1/Common Child.cpp
@@ -1,5 +1,30 @@
 // Question Link: https://www.hackerrank.com/challenges/common-child/problem
 
+// Top-Down
+int helper(int i, int j, string &a, string &b, vector<vector<int>> &memo) {
+    if (memo[i][j] != -1) {
+        return memo[i][j];
+    }
+    if (i == a.size() || j == b.size()) {
+        return 0;
+    }
+    int result = 0;
+    if (a[i] == b[j]) {
+        result = 1 + helper(i + 1, j + 1, a, b, memo);
+    }
+    else {
+        result = max(helper(i, j + 1, a, b, memo), helper(i + 1, j, a, b, memo));
+    }
+    memo[i][j] = result;
+    return result;
+}
+
+int commonChild(string a, string b) {
+    vector<vector<int>> memo(a.size() + 1, vector<int>(b.size() + 1, -1));
+    return helper(0, 0, a, b, memo);
+}
+
+// Bottom-Up
 int commonChild(string a, string b) {
     int aLen = a.size(), bLen = b.size();
     vector<vector<int>> dp(aLen + 1, vector<int>(bLen + 1, 0));

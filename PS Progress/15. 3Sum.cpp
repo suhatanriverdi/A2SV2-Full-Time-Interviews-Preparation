c78405e66d45034316455cbd9f518a7754b95d12
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-04-26 18:50:27 +0300

    Update 15. 3Sum.cpp

--- PS Progress/15. 3Sum.cpp
+++ PS Progress/15. 3Sum.cpp
@@ -1,5 +1,42 @@
 // Question Link: https://leetcode.com/problems/3sum
 
+// 56ms without BinarySearch
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int N = nums.size();
+        vector<vector<int>> result;
+        if (N < 2) {
+            result;
+        }
+        sort(begin(nums), end(nums));
+        for (int i = 0; i < N - 2 && nums[i] <= 0; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int j = i + 1;
+            int k = N - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < 0) {
+                    j++;
+                }
+                else if (sum > 0) {
+                    k--;
+                }
+                else {
+                    result.push_back({nums[i], nums[j], nums[k]});
+                    k--;
+                    while (j < k && nums[k] == nums[k + 1]) {
+                        k--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};
+
 // 336ms Without Using Set
 class Solution {
 public:

a6cc437dce0cc651e6c06547eb55ab7a27d50e5d
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-04-26 14:25:21 +0300

    Update 890. Find and Replace Pattern.cpp

--- PS Progress/890. Find and Replace Pattern.cpp
+++ PS Progress/890. Find and Replace Pattern.cpp
@@ -1,5 +1,6 @@
 // Question Link: https://leetcode.com/problems/find-and-replace-pattern/
 
+// Alternative Solution
 class Solution {
 public:
     vector<string> findAndReplacePattern(vector<string> &words, string pattern) {
@@ -31,5 +32,35 @@ public:
             pattern += patternMap[c];
         }
         return pattern;
+    }
+};
+
+// Two Maps Solution
+class Solution {
+public:
+    vector<string> findAndReplacePattern(vector<string> &words, string pattern) {
+        vector<string> result;
+        for (string &word : words) {
+            if (matches(pattern, word)) {
+                result.push_back(word);
+            }
+        }
+        return result;
+    }
+    
+    bool matches(string &p, string &w) {
+        unordered_map<char, char> wordMap, patternMap;
+        for (int i = 0; i < w.size(); i++) {
+            if (wordMap.find(w[i]) == wordMap.end()) {
+                wordMap[w[i]] = p[i];
+            }
+            if (patternMap.find(p[i]) == patternMap.end()) {
+                patternMap[p[i]] = w[i];
+            }
+            if (wordMap[w[i]] != p[i] || patternMap[p[i]] != w[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 };

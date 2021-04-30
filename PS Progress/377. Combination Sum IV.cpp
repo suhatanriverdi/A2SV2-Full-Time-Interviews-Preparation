cb8102f8e10f1ff79edc9a8e146ca0c6f310da39
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-04-30 23:46:22 +0300

    Update 377. Combination Sum IV.cpp

--- PS Progress/377. Combination Sum IV.cpp
+++ PS Progress/377. Combination Sum IV.cpp
@@ -1,5 +1,32 @@
 // Question Link: https://leetcode.com/problems/combination-sum-iv/
 
+// 0ms without using map
+class Solution {
+    int memo[20001];
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        memset(memo, -1, sizeof(memo));
+        return dfs(nums, target);
+    }
+    
+    int dfs(vector<int> &nums, int sum) {
+        if (memo[sum] != -1) {
+            return memo[sum];
+        }
+        if (sum <= 0) {
+            return sum == 0;
+        }
+        int result = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (sum - nums[i] >= 0) {
+                result += dfs(nums, sum - nums[i]);
+            }
+        }
+        memo[sum] = result;
+        return result;
+    }
+};
+
 // 4ms DP without sorting
 class Solution {
     unordered_map<int, int> memo;

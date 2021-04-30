d146ac74145a435262015de9cd776cda4147647f
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-04-30 22:31:55 +0300

    Add files via upload

--- PS Progress/329. Longest Increasing Path in a Matrix.cpp
+++ PS Progress/329. Longest Increasing Path in a Matrix.cpp
@@ -2,61 +2,33 @@
 
 class Solution {
 private:
-    vector<vector<int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}}, memo[201][201];
 public:
-    int longestIncreasingPath(vector<vector<int>>& matrix) {
-        int M = matrix.size(), N = matrix[0].size();
-        // We only need to have end nodes of the possible paths
-        queue<vector<int>> endNodes;
-        // This keeps track of the path lenghts during the BFS and visited case
-        vector<vector<int>> maxSeen(M, vector<int>(N, 0));
-        // Find the nodes that has no outgoing edges
-        for (int r = 0; r < M; r++) {
-            for (int c = 0; c < N; c++) {
-                if (isEndNode(matrix, r, c)) {
-                    endNodes.push({r, c});
-                    // Mark the starting lenght as 1 at the end of paths
-                    maxSeen[r][c] = 1;
-                }
+    int longestIncreasingPath(vector<vector<int>> &matrix) {
+        int M = matrix.size();
+        int N = matrix[0].size(), longestPath = 0;
+        memset(memo, -1, sizeof(memo));
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                longestPath = max(longestPath, dfs(i, j, matrix));
             }   
         }
-        int longestPathLenght = 1;
-        // Run BFS to get longest path lenght
-        while (!endNodes.empty()) {
-            vector<int> cur = endNodes.front();
-            endNodes.pop();
-            // Check our 4 neighbors, keep going from smaller ones
-            for (vector<int> &dir : dirs) {
-                int nx = dir[0] + cur[0];
-                int ny = dir[1] + cur[1];
-                // If our neighbor is smaller than us and not visited yet
-                if (inside(matrix, nx, ny) && matrix[nx][ny] < matrix[cur[0]][cur[1]] && maxSeen[cur[0]][cur[1]] + 1 > maxSeen[nx][ny]) {
-                    endNodes.push({nx, ny});
-                    maxSeen[nx][ny] = maxSeen[cur[0]][cur[1]] + 1;
-                    longestPathLenght = max(longestPathLenght, maxSeen[nx][ny]);
-                }
-            }
-        }
-        return longestPathLenght;
+        return longestPath;
     }
     
-    // Check if we are bigger than our 4 directionally neighbors
-    bool isEndNode(vector<vector<int>> &M, int &x, int &y) {
-        for (vector<int> &dir : dirs) {
-            int nx = dir[0] + x;
-            int ny = dir[1] + y;
-            if (inside(M, nx, ny) && M[x][y] < M[nx][ny]) {
-                return false;
+    int dfs(int i, int j, vector<vector<int>> &M) {
+        if (memo[i][j] != -1) {
+            return memo[i][j];
+        }
+        int result = 1;
+        for (auto dir : directions) {
+            int ni = dir[0] + i;
+            int nj = dir[1] + j;
+            if (ni >= 0 && ni < M.size() && nj >= 0 && nj < M[0].size() && M[ni][nj] > M[i][j]) {
+                result = max(dfs(ni, nj, M) + 1, result);
             }
         }
-        return true;
-    }
-    
-    // Check if we are inside
-    bool inside(vector<vector<int>> &M, int &x, int &y) {
-        if (x < 0 || x >= M.size() || y < 0 || y >= M[0].size()) {
-            return false;
-        }
-        return true;
+        memo[i][j] = result;
+        return result;
     }
 };

e370f332f7e75baa551828f694964773b65bde34
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-05-04 14:19:18 +0300

    Add files via upload

---
+++ PS Progress/515. Find Largest Value in Each Tree Row.cpp
@@ -0,0 +1,24 @@
+// Question Link: https://leetcode.com/problems/find-largest-value-in-each-tree-row/
+
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        int level = 0, maxLevel = -1;
+        vector<int> result, map(1000, INT_MIN);
+        dfs(root, level, maxLevel, map);
+        for (int i = 0; i <= maxLevel; i++) {
+            result.push_back(map[i]);
+        }
+        return result;
+    }
+    
+    void dfs(TreeNode *cur, int level, int &maxLevel, vector<int> &map) {
+        if (cur == NULL) {
+            return;
+        }
+        maxLevel = max(maxLevel, level);
+        map[level] = max(map[level], cur->val);
+        dfs(cur->right, level + 1, maxLevel, map);
+        dfs(cur->left, level + 1, maxLevel, map);
+    }
+};

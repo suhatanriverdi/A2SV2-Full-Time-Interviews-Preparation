6d6b739dbaf90ecd3786eef796965f67a3c22797
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-05-10 20:36:30 +0300

    Update 81. Search in Rotated Sorted Array II.cpp

--- PS Progress/81. Search in Rotated Sorted Array II.cpp
+++ PS Progress/81. Search in Rotated Sorted Array II.cpp
@@ -1,5 +1,48 @@
 // Question Link: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
 
+// Iterative Solution
+class Solution {
+public:
+    bool search(vector<int> nums, int target) {
+        int L = 0, R = nums.size() - 1, M;
+        while (L <= R) {
+            M = (L + R) / 2;
+            if (nums[M] == target) {
+                return true;
+            }
+            // Edge Case, we don't know which part is properly sorted
+            if (nums[L] == nums[M] and nums[M] == nums[R]) {
+                L++;
+                R--;
+                continue;
+            }
+            // If Left is sorted
+            if (nums[L] <= nums[M]) {
+                // If the target lays on the left part
+                if (nums[L] <= target and target <= nums[M]) {
+                    R = M - 1;
+                } 
+                else {
+                    L = M + 1;
+                }
+            }
+            // If Right is sorted
+            else {
+                // If the target lays on the right part
+                if (nums[M] <= target and target <= nums[R]) {
+                    L = M + 1;
+                } 
+                else {
+                    R = M - 1;
+                }
+            }
+        }   
+        return false;
+    }
+};
+
+
+// Recursion Solution
 class Solution {
 public:
     bool search(vector<int> nums, int target) {
@@ -37,4 +80,3 @@ public:
         return false;
     }
 };
-

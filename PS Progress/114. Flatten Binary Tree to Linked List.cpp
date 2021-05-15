c3f03921073b0a1039a75eeaea3d765ad63d07f6
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-05-15 15:33:49 +0300

    Update 114. Flatten Binary Tree to Linked List.cpp

--- PS Progress/114. Flatten Binary Tree to Linked List.cpp
+++ PS Progress/114. Flatten Binary Tree to Linked List.cpp
@@ -1,5 +1,52 @@
 // https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
 
+// O(1) Solution
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if (root == NULL) {
+            return;
+        }
+        TreeNode* R = root;
+        while (R) {
+            // Move left subtree to the right
+            if (R->left) {
+                TreeNode* ListPointer = R;
+                moveToRight(R->left, &ListPointer, root);
+                R->left = NULL;
+            }
+            R = R->right;
+        }
+    }
+    
+    void updateListPointer(TreeNode **ListPointer) {
+        *ListPointer = (*ListPointer)->right;
+    }
+    
+    // Pre-oerder traversal
+    void moveToRight(TreeNode* cur, TreeNode** ListPointer, TreeNode* root) {
+        if (cur == NULL) {
+            return;
+        }
+        // Save right subtree
+        TreeNode* tmp = (*ListPointer)->right;
+        
+        // Move current element to the right
+        (*ListPointer)->right = new TreeNode(cur->val);
+        
+        // Move right subtree back
+        (*ListPointer)->right->right = tmp;
+        (*ListPointer)->right->left = NULL;
+        
+        // Update Listpointer
+        updateListPointer(&(*ListPointer));
+        
+        moveToRight(cur->left, ListPointer, root);
+        moveToRight(cur->right, ListPointer, root);
+    }
+};
+
+// O(N) Solution
 class Solution {
 public:
     void flatten(TreeNode* root) {

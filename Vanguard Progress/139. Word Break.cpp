86e1fe26517af3584e6d3ed09e2d5ff31f771220
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-08-05 20:10:40 +0300

    Update 139. Word Break.cpp

--- Vanguard Progress/139. Word Break.cpp
+++ Vanguard Progress/139. Word Break.cpp
@@ -1,5 +1,42 @@
 // Question Link: https://leetcode.com/problems/word-break
 
+// 0 ms
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict(begin(wordDict), end(wordDict));
+        set<int> sizes;
+        for (string &word : wordDict) {
+             sizes.insert(word.size());
+        }
+        int memo[301] = { -1 };
+        memset(memo, -1, sizeof(memo));
+        return helper(0, s, dict, sizes, memo);
+    }
+    
+    int helper(int pos, string &s, unordered_set<string> &dict, set<int> &sizes, int memo[]) {
+        if (memo[pos] != -1) {
+            return memo[pos];
+        }
+        if (pos == s.size()) {
+            return 1;
+        }
+        bool found = 0;
+        for (auto &len : sizes) {
+            int width = pos + len - 1;
+            if (width >= s.size()) {
+                break;
+            }
+            string sub = s.substr(pos, len);
+            if (dict.find(sub) != dict.end()) {
+                found |= helper(width + 1, s, dict, sizes, memo);
+            }
+        }
+        memo[pos] = found;
+        return found;
+    }
+};
+
 // A bit optimized
 class Solution {
 public:

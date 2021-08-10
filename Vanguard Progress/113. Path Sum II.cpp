3bf935b3cea883d581d9d16381e468d390c4dea8
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-08-10 14:07:14 +0300

    Update 113. Path Sum II.cpp

--- Vanguard Progress/113. Path Sum II.cpp
+++ Vanguard Progress/113. Path Sum II.cpp
@@ -11,6 +11,35 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// With Backtracking 4ms
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> result;
+        vector<int> path;
+        dfs(root, targetSum, path, result);
+        return result;
+    }
+    
+    void dfs(TreeNode* node, int targetSum, vector<int> &path, vector<vector<int>> &result) {
+        if (!node) {
+            return;
+        }
+        
+        targetSum -= node->val;
+        path.push_back(node->val);
+        if (!node->left && !node->right && targetSum == 0) {
+            result.push_back(path);
+        }
+        
+        dfs(node->left, targetSum, path, result);
+        dfs(node->right, targetSum, path, result);
+        path.pop_back();
+    }
+};
+
+// Without Backtracking 16ms
 class Solution {
 public:
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
